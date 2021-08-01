66d99fa8506b41143ee783fe21d52a50165dbb7e
SudoWatson <aulennert@gmail.com>
2021-07-31 19:09:52 -0500

    Modified update bash

--- update.bash
+++ update.bash
@@ -1,11 +1,7 @@
-workingDir=$(pwd)
-
-cd $1
-
 git pull https://github.com/SudoWatson/spotifyDownload
 
 .venv/Scripts/python.exe -m pip install --upgrade pip
 
 .venv/Scripts/python.exe -m pip install -r requirements.txt
 
-cd $wd
+echo "Finished gathering packages"
